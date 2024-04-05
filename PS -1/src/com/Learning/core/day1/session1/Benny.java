fe84d1a6b7afbfcb09f8e3a21cfc407c3791afb5
Bennydakka <99865301+Bennydakka@users.noreply.github.com>
2024-04-05 17:02:09 +0530

    classes and objects in java

---
+++ PS -1/src/com/Learning/core/day1/session1/Benny.java
@@ -0,0 +1,17 @@
+package com.Learning.core.day1.session1;
+
+public class Benny {
+	public static Book CreateBooks(String book_title,Double price) {
+          Book book1=new Book();
+          book1.setBook_title(book_title);
+          book1.setPrice(price);
+          return book1;
+}
+	public static void showBooks(Book book) {
+		System.out.println("Book Title: "+book.getbook_title()+"  "+"and price: "+book.getPrice());
+		}
+	public static void main(String[] args) {
+		Book book2=CreateBooks("java ",3500.00);
+		showBooks(book2);
+	}
+}
---
+++ PS -1/src/com/Learning/core/day1/session1/Book.java
@@ -0,0 +1,20 @@
+package com.Learning.core.day1.session1;
+
+public class Book {
+	private String book_title;
+	private double price;
+	public String getbook_title() {
+		return book_title;
+		
+	}
+  public void setBook_title(String book_title) {
+	  this.book_title = book_title;
+	  
+  }
+  public Double getPrice() {
+	  return price;
+  }
+  public void setPrice(Double price) {
+	  this.price = price;
+  }
+}
